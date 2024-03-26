f892786c41ebf91b7c88503cb878a2b1dc141753
bdkha <buidinhkha23@gmail.com>
2024-03-26 20:26:37 +0700

    update: faucet api

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 // Create a new instance of axios with the base URL from .env
 export const api = axios.create({
-  baseURL: 'http://13.215.62.47:3030',
+  baseURL: 'http://localhost:3030',
   headers: {
     'Content-Type': 'application/json',
   },
